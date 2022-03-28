0a70684c8b4d3d4fd5217ff78fa10cee5867ddc2
Francesco Ponzio <fr.ponzio@gmail.com>
2022-03-28 09:15:48 +0200

    added source code

---
+++ TTaWrapper.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+from resnet import ResNet
+
+
+class TTAWrapper(tf.keras.Model):
+    def __init__(self, num_classes=10, input_shape=(32, 32, 3)):
+        super(TTAWrapper, self).__init__()
+        self.model = ResNet()
+        self._input_shape = input_shape
+        self._uncertainty_metrics = dict()
+        self._uncertainty_metrics['entropy'] = self._entropy
+        self._uncertainty_metrics['variance'] = self._variance
+        self._uncertainty_metrics['max_softmax_response'] = self._max_softmax_response
+
+    def call(self, inputs):
+        return self.model(inputs)
+
+    def predict_many_times(self, ds, policy, repeat=1):
+        pred_augmented = list()
+
+        policy_random_crop = [
+            tf.keras.Sequential([
+                tf.keras.layers.RandomCrop(int(self.input_shape[0] - 0.1*self.input_shape[0]),
+                                           int(self.input_shape[0] - 0.1*self.input_shape[0])),
+                tf.keras.layers.Resizing(self.input_shape[0],
+                                         self.input_shape[1])])]
+
+        policy_random_flip = [
+            tf.keras.Sequential([
+                tf.keras.layers.RandomFlip("horizontal")])
+        ]
+
+        policy_random_contrast = [
+            tf.keras.Sequential([
+                tf.keras.layers.RandomContrast(0.3, 0.3)])
+        ]
+
+        policy_random_zoom = [
+            tf.keras.Sequential([
+                tf.keras.layers.RandomZoom(0.3)])
+        ]
+
+        policy_identity = [tf.keras.Sequential([
+            tf.keras.layers.Layer()])]
+
+        policies = dict()
+        policies['identity'] = policy_identity
+        policies['random_crop'] = policy_random_crop
+        policies['random_contrast'] = policy_random_contrast
+        policies['random_zoom'] = policy_random_zoom
+        policies['random_flip'] = policy_random_flip
+
+        policies['random_crop'] = sum([policies['random_crop'] for _ in range(repeat)], [])
+        policies['random_contrast'] = sum([policies['random_contrast'] for _ in range(repeat)], [])
+        policies['random_zoom'] = sum([policies['random_zoom'] for _ in range(repeat)], [])
+        policies['random_flip'] = sum([policies['random_flip'] for _ in range(repeat)], [])
+
+        for p in policies[policy]:
+            ds_augmented = ds.map(lambda x, y: (p(x, training=True), y))
+            pred_augmented.append(self.predict(ds_augmented))
+
+        return pred_augmented
---
+++ resnet.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+
+
+class Residual(tf.keras.Model):
+    """The Residual block of ResNet."""
+    def __init__(self, num_channels, use_1x1conv=False, strides=1):
+        super().__init__()
+        self.conv1 = tf.keras.layers.Conv2D(
+            num_channels,
+            padding='same',
+            kernel_size=3,
+            strides=strides)
+        self.conv2 = tf.keras.layers.Conv2D(
+            num_channels,
+            kernel_size=3,
+            padding='same')
+        self.conv_1x1 = None
+        if use_1x1conv:
+            self.conv_1x1 = tf.keras.layers.Conv2D(
+                num_channels,
+                kernel_size=1,
+                strides=strides)
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.bn2 = tf.keras.layers.BatchNormalization()
+
+    def call(self, x):
+        y = tf.keras.activations.relu(self.bn1(self.conv1(x)))
+        y = self.bn2(self.conv2(y))
+        if self.conv_1x1 is not None:
+            x = self.conv_1x1(x)
+        y += x
+        return tf.keras.activations.relu(y)
+
+
+class ResnetBlock(tf.keras.layers.Layer):
+    def __init__(self, num_channels, num_residuals, downscale=True,
+                 **kwargs):
+        super(ResnetBlock, self).__init__(**kwargs)
+        self.residual_layers = []
+        for i in range(num_residuals):
+            if i == 0 and downscale:
+                self.residual_layers.append(
+                    Residual(num_channels, use_1x1conv=True, strides=2))
+            else:
+                self.residual_layers.append(Residual(num_channels))
+
+    def call(self, x):
+        for layer in self.residual_layers.layers:
+            x = layer(x)
+        return x
+
+
+class ResNet(tf.keras.Model):
+
+    def __init__(self, num_classes=10, augment=False):
+        super(ResNet, self).__init__()
+        self.num_classes = num_classes
+        self.augment = augment
+        self.augmentation_block = tf.keras.Sequential([
+            tf.keras.layers.Resizing(112, 112),
+            tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+            tf.keras.layers.RandomZoom(
+                height_factor=(-0.05, -0.15),
+                width_factor=(-0.05, -0.15)),
+            tf.keras.layers.RandomRotation(0.3)])
+        self.block_a = tf.keras.Sequential(
+            [tf.keras.layers.Conv2D(64,
+                                    kernel_size=7,
+                                    strides=2,
+                                    padding='same'),
+             tf.keras.layers.BatchNormalization(),
+             tf.keras.layers.MaxPool2D(pool_size=3,
+                                       strides=2,
+                                       padding='same')])
+        self.block_b = ResnetBlock(64, 2, downscale=False)
+        self.block_c = ResnetBlock(128, 2)
+        self.block_d = ResnetBlock(256, 2)
+        self.block_e = ResnetBlock(512, 2)
+        self.global_pool = tf.keras.layers.GlobalAveragePooling2D()
+        self.classifier = tf.keras.layers.Dense(self.num_classes, activation="softmax")
+
+
+def call(self, inputs):
+    if self.augment:
+        x = self.augmentation_block(inputs)
+        x = self.block_a(x)
+    else:
+        x = self.block_a(inputs)
+    x = self.block_b(x)
+    x = self.block_c(x)
+    x = self.block_d(x)
+    x = self.block_e(x)
+    x = self.global_pool(x)
+    x = tf.keras.layers.Dropout(0.25)(x)
+    return self.classifier(x)
