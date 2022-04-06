d0bafee2d200a7717d1344955ee1eb4087f6c1e3
Francesco Ponzio <fr.ponzio@gmail.com>
2022-04-06 12:38:27 +0200

    annotation mask ok - fix 1

--- process_WSIs.py
+++ process_WSIs.py
@@ -7,6 +7,11 @@ os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = 'true'
 rootdir_wsi = "/space/ponzio/CRC_WSIs/"
 rootdir_src = "/space/ponzio/teaching-MLinAPP/src/"
+save_dir = os.path.join(rootdir_wsi, "CRC_WSIs_tiles")
+try:
+    os.makedirs(save_dir)
+except FileExistsError:
+    print("{} exists.".format(save_dir))
 sys.path.append(rootdir_src)
 from wsi_utils import DatasetManager
 from wsi_utils import get_heatmap
@@ -45,26 +50,11 @@ for wsi, label in zip(wsi_file_paths_test, wsi_labels_numerical_test):
                                           batch_size=batch_size)
     dataset_test = dataset_manager_test.make_dataset(shuffle=False)
     preds = model.predict(dataset_test)
-    tile_placeholders = dataset_manager_test.get_tile_placeholders_filt
+    tile_placeholders = dataset_manager_test.get_tile_placeholders
     for tile, pred in zip(tile_placeholders, preds):
         tile['prediction'] = pred
     filename = os.path.basename(wsi).split('.')[0]
     filepath = os.path.join(rootdir_wsi, "{}_tile_placeholders.pickle".format(filename))
     with open(filepath, "wb") as fp:
         pickle.dump(tile_placeholders, fp)
-
-    heatmap_NH = get_heatmap(tile_placeholders,
-                             class_to_map=1,
-                             level_downsample=1/50,
-                             num_classes=2,
-                             colormap=matplotlib.cm.get_cmap('Reds'),
-                             tile_placeholders_mapping_key='prediction')
-    heatmap_NH.save(os.path.join(rootdir_wsi, "{}-NH.png".format(filename)))
-    heatmap_H = get_heatmap(tile_placeholders,
-                            class_to_map=0,
-                            level_downsample=1/50,
-                            num_classes=2,
-                            colormap=matplotlib.cm.get_cmap('Blues'),
-                            tile_placeholders_mapping_key='prediction')
-    heatmap_H.save(os.path.join(rootdir_wsi, "{}-H.png".format(filename)))
     j += 1
