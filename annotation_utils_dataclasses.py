9eafa6f125226819a5f9377212d33ef16dfeaf8e
Samuel Oreste Abreu <orestsam@protonmail.com>
2022-06-20 13:12:24 +0200

    Fix: potential fix for all invalid polygons

--- annotation_utils_dataclasses.py
+++ annotation_utils_dataclasses.py
@@ -11,6 +11,10 @@ class AnnotationData:
     polygon: Polygon = field(init=False)
     def __post_init__(self):
         self.polygon = Polygon(self.points)
+        if not self.polygon.is_valid:
+            #https://stackoverflow.com/questions/20833344/fix-invalid-polygon-in-shapely
+            #Fixes intersecting lines which render the polygon invalid
+            self.polygon = self.polygon.buffer(0)
 
 @dataclass
 class PointInfo:
